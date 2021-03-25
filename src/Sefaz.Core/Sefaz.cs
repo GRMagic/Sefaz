9e4d62f38a91383e01be25997c8bf0460b7021f4
Gustavo Rossi Müller <gustavo.rm90@gmail.com>
2021-03-25 19:57:45 -0300

    Criado projeto core

---
+++ src/Sefaz.Core/Sefaz.cs
@@ -0,0 +1,14 @@
+﻿namespace Sefaz.Core
+{
+    public static class Sefaz
+    {
+        /// <summary>
+        /// Método criado apenas para testar o pacote nuget
+        /// </summary>
+        /// <returns>Pong</returns>
+        public static string Ping()
+        {
+            return "Pong";
+        }
+    }
+}
